0784e052d47de9205b220c682cb99265a048cc4c
menzorg <menzorg@gmail.com>
2022-03-22 03:54:45 +0300

    NEXT_PUBLIC_ENGINES_ROUTE env

--- pages/api/deeplinks.ts
+++ pages/api/deeplinks.ts
@@ -1,10 +1,10 @@
 import { call } from '@deep-foundation/deeplinks/imports/engine';
 
-const NEXT_PUBLIC_ENGINES = process.env.NEXT_PUBLIC_ENGINES || '0';
+const NEXT_PUBLIC_ENGINES_ROUTE = process.env.NEXT_PUBLIC_ENGINES_ROUTE || '1';
 
 export default async (req, res) => {
   const PATH = [];
-  if (!+NEXT_PUBLIC_ENGINES) return res.send('engines deactivated');
+  if (!+NEXT_PUBLIC_ENGINES_ROUTE) return res.send('engines deactivated');
   if (req?.body?.envs?.PATH) PATH.push(req?.body?.envs?.PATH);
   if (process?.env?.PATH) PATH.push(process?.env?.PATH);
   res.send(await call({ ...req?.body, envs: { ...req?.body?.envs, PATH: PATH.join(':')}}));
