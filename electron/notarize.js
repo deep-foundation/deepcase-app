e3d7f01bc4bd17514335b03a1e79ecaf49a47927
menzorg <menzorg@gmail.com>
2022-03-29 16:51:58 +0300

    build fix

--- electron/notarize.js
+++ electron/notarize.js
@@ -1,18 +1,22 @@
 const { notarize } = require('electron-notarize');
 const { signAsync } = require ('electron-osx-sign');
 
-const appleIdPassword = `rfzy-cowy-owqm-zocn`;
 const appPath = '/Users/menzorg/dev/packages/deepcase/electron/dist/mac/Deep.Case.app'
 const appBundleId = 'deep.case.app';
 const appleId = 'menzorg@gmail.com';
 
-exports.default = async function packageTask () {
+exports.default = async function packageTask (context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== "darwin") {
+    return;
+  }
+  const appName = context.packager.appInfo.productFilename;
   console.log('notarizing');
   return await notarize({
     appBundleId,
-    appPath,
-    appleId,
-    appleIdPassword,
+    appPath: `${appOutDir}/${appName}.app`,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
   });
   console.log('done');
 }
