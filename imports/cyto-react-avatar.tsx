c2f1f55a55722a7fd7890de7c9177076136ecc75
Aruseli <llingveri@gmail.com>
2022-07-14 20:10:03 +0000

    message bubble

---
+++ imports/cyto-react-avatar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Avatar } from './framework';
+
+
+export const CytoReactLinkAvatar = React.memo<any>(({
+  emoji,
+  name,
+  src,
+}: {
+  emoji?: string;
+  name?: string;
+  src?: string;
+}) => {
+  return src
+    ? <Avatar src={src} />
+    : emoji
+    ? <Avatar name={emoji} getInitials={str => str} />
+    : name
+    ? <Avatar name={name} />
+    : <Avatar name={'?'} />
+});
---
+++ pages/message.tsx
@@ -0,0 +1,37 @@
+
+import { Link, useMinilinksConstruct, useMinilinksFilter } from '@deep-foundation/deeplinks/imports/minilinks';
+import dynamic from "next/dynamic";
+import { ConnectionController } from '.';
+import { ColorModeSwitcher } from '../imports/color-mode-toggle';
+import { CytoGraphProps } from '../imports/cyto-graph-props';
+import { CytoReactMessage } from '../imports/cyto-react-message';
+import { DeepLoader } from '../imports/loader';
+import { Provider } from '../imports/provider';
+
+const CytoGraph = dynamic<CytoGraphProps>(
+  () => import('../imports/cyto-graph-react').then((m) => m.default),
+  { ssr: false }
+);
+
+export default function Page() {
+  // const [spaceId, setSpaceId] = useSpaceId();
+  const spaceId = 234;
+  const minilinks = useMinilinksConstruct();
+  const { ref: mlRef, ml } = minilinks;
+  const links: Link<number>[] = useMinilinksFilter(ml, (l) => true, (l, ml) => [...ml.links]);
+
+  return (<>
+    <Provider chakra>
+      <ConnectionController>
+        {[<DeepLoader
+          key={spaceId}
+          spaceId={spaceId}
+          minilinks={minilinks}
+          // onUpdateScreenQuery={query => console.log('updateScreenQuery', query)}
+          />]}
+        <ColorModeSwitcher/>
+        <CytoReactMessage />
+      </ConnectionController>
+    </Provider>
+  </>);
+}
