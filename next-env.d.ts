497aae91011c7d6a358e2f81e622faada1af3030
Ivan S Glazunov <ivansglazunov@gmail.com>
2022-08-01 05:41:09 +0300

    update next

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module '*.mp3' {
-  const src: string;
-  export default src;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
