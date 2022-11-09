6d0734ff0d69b74cbf5aa4ab26bcf5883c0fa857
Anastasia <llingveri@gmail.com>
2022-11-09 22:23:52 +0300

    component spawn animation

--- imports/component-appearance.tsx
+++ imports/component-appearance.tsx
@@ -1,19 +1,44 @@
-import React, { ReactNode } from 'react';
 import { motion, useAnimation } from 'framer-motion';
+import React, { ReactNode, useEffect } from 'react';
 
 
 const variants = {
   show: {
-    
+    // scale: 1,
+    scaleX: 1,
+    scaleY: 1,
+    opacity: 1,
+    // width: '100%',
+    // height: '100%',
+    borderRadius: '0%',
+    transition: { duration: 0.5 }
+  },
+  hide: {
+    scaleX: 0.3,
+    scaleY: 0.1,
+    opacity: 0,
+    borderRadius: '50%',
+    transition: { 
+      // scale: { delay: 1 },
+      duration: 0.8 
+    }
   }
 }
 
-export const Appearance = React.memo<any>(({children}:{children: ReactNode;}) => {
+export const Appearance = React.memo<any>(({children, toggle}:{children: ReactNode; toggle?: boolean;}) => {
   const control = useAnimation();
+  useEffect(() => {
+    if (toggle === true) {
+      control.start("show"); 
+    } else {
+      control.start("hide");
+    }
+  }, [control, toggle]);
 
   return (<motion.div
       variants={variants}
       animate={control}
+      style={{overflow: 'hidden', position: 'relative', width: 'max-content', height: '100%'}}
     >
       {children}
     </motion.div>
