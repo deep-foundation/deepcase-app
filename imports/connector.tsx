7bb5b4878905379109d91c970169ccfe36a5ef74
Anastasia <llingveri@gmail.com>
2022-11-19 23:37:12 +0300

    connector

--- imports/connector.tsx
+++ imports/connector.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
 import { Box, Button, Divider, Flex, IconButton, Image, Input, InputGroup, InputLeftAddon, InputLeftElement, InputRightAddon, InputRightElement, Spacer, Text } from '@chakra-ui/react';
 import { ModalWindow } from "./modal-window";
 import { useDebounceCallback } from "@react-hook/debounce";
@@ -68,18 +68,46 @@ const Initializing = React.memo<any>(() => {
   )
 });
 
-
+const Initialized = React.memo(() => {
+  return (<Flex width='100%' justify='space-between' pt={2} pb={2} alignItems='center'>
+      <IconButton
+        variant='unstyled' 
+        size='md'
+        aria-label='Add local route' 
+        icon={
+          <IoPlayOutline color='rgb(0, 128, 255)' />
+        } 
+      />
+      <Text sx={{color: '#F7FAFC'}} fontSize='sm' as='kbd' mr='0.125rem'>Initializing</Text>
+      <IconButton
+        variant='unstyled' 
+        size='md'
+        aria-label='Add local route' 
+        icon={
+          <MdDelete color='rgb(0, 128, 255)' />
+        } 
+      />
+      
+    </Flex>
+  )
+})
 
 const inputArea = {
   open: {
     height: '5rem',
+    // scaleY: 1,
+    // display: 'block',
+    transition: { duration: 0.5 }
   }, 
   close: {
     height: '0rem',
+    // scaleY: 0,
+    // display: 'none',
     transition: { delay: 0.5 }
   },
   initial: {
     height: '0rem',
+    // scaleY: 0,
     originY: 1
   }
 };
@@ -89,7 +117,7 @@ const inputAnimation = {
     opacity: 1,
     scaleY: 1,
     originY: 0,
-    display: 'flex',
+    // display: 'flex',
     transition: { 
       duration: 0.5,
       delay: 0.2
@@ -99,10 +127,10 @@ const inputAnimation = {
     opacity: 0,
     scaleY: 0,
     originY: 1,
-    display: 'none',
+    // display: 'none',
     transition: { 
-      duration: 0.5,
-      display: { delay: 0.5 }
+      duration: 0.3,
+      display: { delay: 0.7 }
     }
   }
 };
@@ -115,47 +143,54 @@ const InputAnimation = React.memo<any>(({
   setValueRemote,
   onDeleteValue,
   onStartRemoteRoute,
+  key,
 }:{
   bgContainer?: string;
   addRemoteRout?: boolean;
   valueRemoteRoute?: string;
   onChangeValueRemoteRoute
-  setValueRemote: () => any;
+  setValueRemote?: () => any;
   onDeleteValue: () => any;
-  onStartRemoteRoute: () => any;
+  onStartRemoteRoute?: () => any;
+  key?: any;
 }) => {
   const control = useAnimation();
   const controlInput = useAnimation();
 
   useEffect(() => {
     if (!!addRemoteRout) { 
-      controlInput.start('open');
-      control.start("add");
+      control.start('open');
+      controlInput.start("add");
     } else {
-      control.start("hide");
-      controlInput.start('close');
+      controlInput.start("hide");
+      control.start('close');
     }
-  }, [control, controlInput, addRemoteRout]);
+  }, [addRemoteRout]);
 
   return (<Box 
       as={motion.div}
-      animate={controlInput}
+      animate={control}
       initial='initial'
+      exit='close'
       layout
       variants={inputArea}
       bg={bgContainer}
       w='100%'
+      // height='100%'
+      p={4}
+      key={key}
     >
-      <Box p={4}>
-        <AnimatePresence>
+      {/* <Box > */}
+        {/* <AnimatePresence> */}
           <InputGroup 
             size='lg'
-            layout
+            // layout
             as={motion.div}
-            animate={control}
+            animate={controlInput}
             initial='hide'
             exit='hide'
             variants={inputAnimation}
+            key={key}
           >
             <InputLeftElement 
               onClick={onStartRemoteRoute}
@@ -171,12 +206,11 @@ const InputAnimation = React.memo<any>(({
               } 
             />
           </InputGroup>
-        </AnimatePresence>
-      </Box>
+        {/* </AnimatePresence> */}
+      {/* </Box> */}
     </Box>
   )
 });
-
 
 const cardAnimation = {
   grow: {
@@ -212,7 +246,7 @@ const initArea = {
     //     delay: 3 
     //   }}
   },
-  Initializing: {
+  initializing: {
     // display: 'none',
     scaleY: 0,
     originY: 1,
@@ -234,12 +268,25 @@ export const Connector = React.memo<any>(({
   const [valueRemote, setValueRemote] = useState('');
   const [initLocal, setInitLocal] = useState(false);
   const [initedLocal, setInitedLocal] = useState(false);
-  const [addRemote, setAddRemote] = useState(0);
+  // const [addRemote, setAddRemote] = useState(0);
   const onChangeValueRemote = useDebounceCallback((value) => {
     setValueRemote(value);
   }, 500);
   
-  // let r = (Math.random() + 1).toString(36).substring(7);
+  const [remoteRouts, setArr] = useState([]);
+  
+  const add = () => {
+    setArr((remoteRouts) => [
+      ...remoteRouts,
+      { id: (Math.random() + 1).toString(36).substring(7), value: "" }
+    ])
+  };
+  const remove = (id) => {
+    setArr((remoteRouts) => remoteRouts.filter((el) => el.id != id))
+  };
+  const save = (id, value) => {
+    setArr((remoteRouts) => remoteRouts.map((el) => (el.id === id ? { ...el, value } : el)))
+  };
 
   useEffect(() => {
     if (!!portalOpen) {
@@ -250,54 +297,18 @@ export const Connector = React.memo<any>(({
   }, [control, portalOpen]);
 
   useEffect(() => {
-    console.log({initLocal, initedLocal});
     if (initLocal && !initedLocal) { 
       controlNotInit.start('close');
       controlInit.start('open');
     } else if (initLocal && initedLocal) {
-      controlNotInit.start('Initializing');
-      controlInit.start('Initializing');
+      controlNotInit.start('initializing');
+      controlInit.start('initializing');
       controlInited.start('open');
     } else {
       controlNotInit.start("open");
     } 
   }, [initLocal, initedLocal]);
 
-  let inputs = [];
-  // for (let i = 0; i < addRemote; i++) {
-  //   inputs.set(
-  //     <InputGroup 
-  //       size='lg' 
-  //       as={motion.div}
-  //       initial={{ scaleY: 0 }}
-  //       animate={{ scaleY: 1 }}
-  //       exit={{ scaleY: 0 }}
-  //       key={i} 
-  //     >
-  //       <InputLeftElement 
-  //         children={
-  //           <CustomizableIcon Component={IoPlayOutline} value={{color: 'rgb(0, 128, 255)'}} />
-  //         } 
-  //         onClick={() => setValueRemote('')}
-  //       />
-  //       <Input placeholder='rout' />
-  //       <InputRightElement 
-  //         as={motion.div}
-  //         children={
-  //           <CustomizableIcon Component={MdDelete} value={{color: 'rgb(0, 128, 255)'}} />
-  //         } 
-  //         onTap={() => {
-  //           if (addRemote > 0) {
-  //             setAddRemote(addRemote - 1);
-  //           } else {
-  //             setAddRemote(0);
-  //           }
-  //         }}
-  //         // onClick={() => setValueRemote('')}
-  //       />
-  //     </InputGroup>
-  //   );
-  // }
   return (<ModalWindow onClosePortal={onClosePortal} portalOpen={portalOpen}>
       <Box 
         display='flex'
@@ -324,18 +335,19 @@ export const Connector = React.memo<any>(({
           <Box pt={4} pl={4} pr={4} textAlign='left' w='100%'>
             <Text sx={{color: '#F7FAFC'}} fontSize='md'>Remote deep</Text>
           </Box>
-          <InputAnimation 
-            addRemoteRout={addRemote}
-            valueRemoteRoute={valueRemote}
-            onChangeValueRemoteRoute={onChangeValueRemote}
-            setValueRemote={() => setValueRemote('')}
-            onDeleteValue={() => {
-              setValueRemote('');
-              setTimeout(() => {
-                setAddRemote(0);
-            }), 500}}
-            onStartRemoteRoute={() => {}}
-          />
+          <AnimatePresence>
+            {remoteRouts.map(rr => (
+              <InputAnimation 
+                addRemoteRout={!!remoteRouts}
+                valueRemoteRoute={rr.value}
+                onChangeValueRemoteRoute={(e) => save(rr.id, e.target.value)}
+                // setValueRemote={}
+                onDeleteValue={() => remove(rr.id)}
+                // onStartRemoteRoute={() => {}}
+                key={rr.id}
+              />)
+            )}
+          </AnimatePresence>
           <Box pb={4} pl={4} pr={4} width='100%'>
             <IconButton
               as={motion.div} 
@@ -345,11 +357,7 @@ export const Connector = React.memo<any>(({
                 <IoAddOutline color='rgb(0, 128, 255)' />
               }
               // onClick={() => setAddRemote(!addRemote)} 
-              onTap={() => {
-                if (addRemote == 0) {
-                  setAddRemote(addRemote + 1);
-                }
-            }}
+              onTap={add}
             />
             <Divider mb={4} />
             <Text sx={{color: '#F7FAFC'}} fontSize='md'>Local deep</Text>
@@ -362,22 +370,18 @@ export const Connector = React.memo<any>(({
             w='100%'
           > 
             <AnimatePresence>
-              <Box display='flex' w='100%' justifyContent='space-between' alignItems='center' minW='18.75rem' position='relative'>
+              <Box display='flex' w='100%' justifyContent='space-between' alignItems='center' minW='19.75rem' position='relative'>
                 <Box 
                   w='100%' 
                   h='100%' 
-                  // display='flex' 
+                  display='flex' 
                   justifyContent='space-between'
                   alignItems='center'
                   as={motion.div}
                   animate={controlNotInit}
                   initial='initial'
                   variants={initArea}
-                  onClick={() => {
-                    console.log({initLocal});
-                    setInitLocal(!initLocal);
-                    console.log({initLocal});
-                  }} 
+                  onClick={() => setInitLocal(!initLocal)} 
                 >
                   <Text sx={{color: '#F7FAFC'}} fontSize='sm' as='kbd'>no initialized</Text>
                   <IconButton
@@ -401,31 +405,22 @@ export const Connector = React.memo<any>(({
                   animate={controlInit}
                   initial='Initializing'
                   variants={initArea}
-                  onClick={() => {
-                    console.log({initedLocal});
-                    setInitedLocal(!initedLocal);
-                    console.log({initedLocal});
-                  }} 
+                  onClick={() => setInitedLocal(!initedLocal)} 
                 >
                   <Initializing />
                 </Box>
                 <Box 
                   w='100%' 
-                  h='100%' 
-                  display='flex' 
+                  h='100%'  
                   position='absolute'
-                  top={0} left={0}
-                  justifyContent='space-between'
-                  alignItems='center'
+                  top={0} left={-4}
                   as={motion.div}
                   animate={controlInited}
-                  initial='Initializing'
+                  initial='initializing'
                   variants={initArea}
-                  onClick={() => {
-                    setInitLocal(!initLocal);
-                  }} 
+                  onClick={() => setInitLocal(!initLocal)} 
                 >
-                  123
+                  <Initialized />
                 </Box>
               </Box>
             </AnimatePresence>
