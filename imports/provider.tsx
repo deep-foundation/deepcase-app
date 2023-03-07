ba30903dffecaddf00cff1601006c47809d7801b
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-07 13:18:07 +0000

    Remove console.logs

--- imports/provider.tsx
+++ imports/provider.tsx
@@ -28,8 +28,6 @@ export function Provider({
   const ThemeProviderCustom = ChakraProvider;
   const themeCustom = themeChakra;
 
-  console.log({themeCustom});
-  console.log({ThemeProviderCustom});
   return (
     // <Analitics
     //   yandexMetrikaAccounts={[84726091]}
