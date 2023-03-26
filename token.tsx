552e60540ce6b68f5ddf58ade555b0513f6915b2
Aruseli <llingveri@gmail.com>
2023-03-26 18:39:58 +0000

    completion of the token

--- token.tsx
+++ token.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 
 export const Token = () => {
   const [value, setValue] = useState('');
+  const [loader, setLoader] = useState(true);
 
   return (<Box maxW='md' borderColor='gray.400' borderWidth='1px' borderRadius='lg'>
       <Link fontSize='xs' href='https://docs.npmjs.com/creating-and-viewing-access-tokens' p={2} isExternal>
@@ -15,12 +16,22 @@ export const Token = () => {
           value={value} 
           borderColor='gray.300' 
           onChange={(e) => setValue(e.target.value)} />
-          {value && <InputRightElement width='4.2rem' top={2} children={
-            <Button size='xs' bg='blue.200' color='blackAlpha.800' onClick={() => console.log('save')}>
-              save
-            </Button>
-          } 
-        />}
+          {!value === true
+            ? <InputRightElement width='4.2rem' top={2} children={
+                <Button isLoading={true} size='xs' bg='blue.300' color='blackAlpha.800' _hover={{ bg: 'blue.500' }} onClick={() => console.log('save')}>
+                  save
+                </Button>
+                } 
+              />
+            : loader === true
+            ? null
+            : <InputRightElement width='4.2rem' top={2} children={
+                <Button isLoading={true} size='xs' bg='blue.200' color='blackAlpha.800' onClick={() => console.log('save')}>
+                  save
+                </Button>
+                } 
+              />
+          }
       </InputGroup>
     </Box>
   );
