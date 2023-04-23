ccb8bf6e1030b92b775e47685d5f2fd8ee4a2ee0
Alexandr <l4legenda@gmail.com>
2023-04-23 21:43:42 +0500

    feat: new react-cytoscapejs

--- imports/cyto/layouts.tsx
+++ imports/cyto/layouts.tsx
@@ -15,10 +15,10 @@ export const layoutColaPreset = ({isAnimate=false}) => ({
 
   // positioning options
   randomize: false, // use random node positions at beginning of layout
-  avoidOverlap: false, // if true, prevents overlap of node bounding boxes
-  handleDisconnected: false, // if true, avoids disconnected components from overlapping
+  avoidOverlap: true, // if true, prevents overlap of node bounding boxes
+  handleDisconnected: true, // if true, avoids disconnected components from overlapping
   convergenceThreshold: 0.01, // when the alpha value (system energy) falls below this value, the layout stops
-  // nodeSpacing: function( node ){ return 50; }, // extra spacing around nodes
+  nodeSpacing: function( node ){ return 10; }, // extra spacing around nodes
   flow: undefined, // use DAG/tree flow layout if specified, e.g. { axis: 'y', minSeparation: 30 }
   alignment: undefined, // relative alignment constraints on nodes, e.g. {vertical: [[{node: node1, offset: 0}, {node: node2, offset: 5}]], horizontal: [[{node: node3}, {node: node4}], [{node: node5}, {node: node6}]]}
   gapInequalities: undefined, // list of inequality constraints for the gap between the nodes, e.g. [{"axis":"y", "left":node1, "right":node2, "gap":25}]
@@ -26,7 +26,19 @@ export const layoutColaPreset = ({isAnimate=false}) => ({
 
   // different methods of specifying edge length
   // each can be a constant numerical value or a function like `function( edge ){ return 2; }`
-  edgeLength: undefined, // sets edge length directly in simulation
+  edgeLength: function( edge ) {
+    const baseLength = 30; // base edge length
+    const extraLength = 10; // additional length of the edge to take into account the density of connections
+    const sourceNode = edge.source();
+    const targetNode = edge.target();
+
+    // Calculate the number of connected edges for source nodes and target nodes
+    const sourceConnectedEdges = sourceNode.connectedEdges().length;
+    const targetConnectedEdges = targetNode.connectedEdges().length;
+
+    // Increase edge length based on the number of connected edges
+    return baseLength + (sourceConnectedEdges + targetConnectedEdges) * extraLength;
+  },
   edgeSymDiffLength: undefined, // symmetric diff edge length in simulation
   edgeJaccardLength: undefined, // jaccard edge length in simulation
 
@@ -40,7 +52,7 @@ export const layoutD3ForcePreset = ({isAnimate=false, cy}) => ({
   name: 'd3-force',
   animate: isAnimate, // whether to show the layout as it's running; special 'end' value makes the layout animate like a discrete layout
   maxIterations: 0, // max iterations before the layout will bail out
-  maxSimulationTime: 1400, // max length in ms to run the layout
+  maxSimulationTime: 400, // max length in ms to run the layout
   ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
   fixedAfterDragging: false, // fixed node after dragging
   fit: false, // on every layout reposition of nodes, fit the viewport
@@ -51,32 +63,32 @@ export const layoutD3ForcePreset = ({isAnimate=false, cy}) => ({
   // alphaMin: 0.001, // sets the minimum alpha to the specified number in the range [0,1]
   // alphaDecay: 1 - Math.pow(0.001, 1 / 300), // sets the alpha decay rate to the specified number in the range [0,1]
   // alphaTarget: 0, // sets the current target alpha to the specified number in the range [0,1]
-  velocityDecay: 0.6, // sets the velocity decay factor to the specified number in the range [0,1]
-  collideRadius: 1, // sets the radius accessor to the specified number or function
-  collideStrength: 0.7, // sets the force strength to the specified number in the range [0,1]
-  collideIterations: 1, // sets the number of iterations per application to the specified number
+  // velocityDecay: 0.6, // sets the velocity decay factor to the specified number in the range [0,1]
+  // collideRadius: 1, // sets the radius accessor to the specified number or function
+  // collideStrength: 0.7, // sets the force strength to the specified number in the range [0,1]
+  // collideIterations: 1, // sets the number of iterations per application to the specified number
   linkId: function id(d) {
     return d.id;
   }, // sets the node id accessor to the specified function
-  linkDistance: 200, // sets the distance accessor to the specified number or function
+  linkDistance: 300, // sets the distance accessor to the specified number or function
   // linkStrength: function strength(link) {
   //   const sourceNode = cy.getElementById(link.source.id);
   //   const targetNode = cy.getElementById(link.target.id);
   //   return 1 / Math.min(sourceNode.degree(), targetNode.degree());
   // }, // sets the strength accessor to the specified number or function
-  linkIterations: 1, // sets the number of iterations per application to the specified number
-  manyBodyStrength: -1000, // sets the strength accessor to the specified number or function
-  manyBodyTheta: 0.9, // sets the Barnes–Hut approximation criterion to the specified number
-  manyBodyDistanceMin: 1, // sets the minimum distance between nodes over which this force is considered
-  manyBodyDistanceMax: Infinity, // sets the maximum distance between nodes over which this force is considered
-  xStrength: 0.1, // sets the strength accessor to the specified number or function
-  xX: 0, // sets the x-coordinate accessor to the specified number or function
-  yStrength: 0.1, // sets the strength accessor to the specified number or function
-  yY: 0, // sets the y-coordinate accessor to the specified number or function
-  radialStrength: 0.05, // sets the strength accessor to the specified number or function
-  radialRadius: [40],// sets the circle radius to the specified number or function
-  radialX: 0, // sets the x-coordinate of the circle center to the specified number
-  radialY: 0, // sets the y-coordinate of the circle center to the specified number
+  // linkIterations: 1, // sets the number of iterations per application to the specified number
+  manyBodyStrength: -1500, // sets the strength accessor to the specified number or function
+  // manyBodyTheta: 0.9, // sets the Barnes–Hut approximation criterion to the specified number
+  // manyBodyDistanceMin: 1, // sets the minimum distance between nodes over which this force is considered
+  // manyBodyDistanceMax: Infinity, // sets the maximum distance between nodes over which this force is considered
+  // xStrength: 0.1, // sets the strength accessor to the specified number or function
+  // xX: 0, // sets the x-coordinate accessor to the specified number or function
+  // yStrength: 0.1, // sets the strength accessor to the specified number or function
+  // yY: 0, // sets the y-coordinate accessor to the specified number or function
+  // radialStrength: 0.05, // sets the strength accessor to the specified number or function
+  // radialRadius: [40],// sets the circle radius to the specified number or function
+  // radialX: 0, // sets the x-coordinate of the circle center to the specified number
+  // radialY: 0, // sets the y-coordinate of the circle center to the specified number
   // layout event callbacks
   ready: function(){}, // on layoutready
   stop: function(){}, // on layoutstop
