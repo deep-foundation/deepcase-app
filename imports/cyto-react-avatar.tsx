d2212b65f483317166ae385b7d3d447539282d6a
Aruseli <llingveri@gmail.com>
2023-05-12 21:45:41 +0000

    size to avatar

--- imports/cyto-react-avatar.tsx
+++ imports/cyto-react-avatar.tsx
@@ -6,16 +6,18 @@ export const CytoReactLinkAvatar = React.memo<any>(({
   emoji,
   name,
   src,
+  size,
 }: {
   emoji?: string;
   name?: string;
   src?: string;
+  size?: string;
 }) => {
   return src
-    ? <Avatar src={src} />
+    ? <Avatar src={src} size={size} />
     : emoji
-    ? <Avatar name={emoji} getInitials={str => str} />
+    ? <Avatar name={emoji} getInitials={str => str} size={size} />
     : name
-    ? <Avatar name={name} />
-    : <Avatar name={'?'} />
+    ? <Avatar name={name} size={size} />
+    : <Avatar name={'?'} size={size} />
 });
