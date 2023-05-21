c671f35bd1858c23886a773f4aa046334e45f083
Aruseli <llingveri@gmail.com>
2023-05-21 20:56:23 +0000

    moving the mechanics of the wysiwyg to the repository

--- imports/deep-wysiwyg.tsx
+++ imports/deep-wysiwyg.tsx
@@ -1,7 +1,7 @@
 import React, { useState, Ref, PropsWithChildren, useCallback } from 'react';
 import { createEditor, Editor, Element as SlateElement, Transforms } from 'slate';
 import { Slate, Editable, withReact, useSlate } from 'slate-react';
-import { Box, useColorMode } from '@chakra-ui/react';
+import { Box, Heading, useColorMode } from '@chakra-ui/react';
 import { FiBold, FiItalic, FiUnderline, FiCode } from 'react-icons/fi';
 import { TbNumber1, TbNumber2, TbQuote, TbList, TbListNumbers } from 'react-icons/tb';
 import { CiTextAlignJustify, CiTextAlignCenter, CiTextAlignLeft, CiTextAlignRight } from 'react-icons/ci';
@@ -14,57 +14,130 @@ const HOTKEYS = {
   'mod+u': 'underline',
   'mod+`': 'code',
 };
+interface BaseProps {
+  className: string
+  [key: string]: unknown
+};
+type OrNull<T> = T | null;
 
 const LIST_TYPES = ['numbered-list', 'bulleted-list'];
 const TEXT_ALIGN_TYPES = ['left', 'center', 'right', 'justify'];
-
-export const DeepWYSIWYG = React.memo<any>(() => {
+  
+  // Only objects editor.
+export const DeepWysiwyg = React.memo<any>(({ fillSize, style, link, FormattingBlock }) => {
   const initialValue = [
     {
       type: 'paragraph',
       children: [{ text: '' }],
     },
   ];
+  
+  const random = Math.floor(Math.random()*16777215).toString(16);
+  const [color, setColor] = useState(random);
+
   const renderLeaf = useCallback(props => <Leaf {...props} />, []);
-  const renderElement = useCallback(props => <Element {...props} />, []);
+  const renderElement = useCallback(props => <Element {...props} state={color} />, []);
   const [editor] = useState(() => withReact(createEditor()));
 
   const { colorMode } = useColorMode();
+  const [viewSize, setViewSize] = useState({width: 300, height: 300});
 
+// const randomBorderColor = Math.floor(Math.random()*16777215).toString(16);
+
+  const Leaf = ({ attributes, children, leaf }) => {
+    if (leaf.bold) {
+      children = <strong>{children}</strong>
+    }
+
+    if (leaf.code) {
+      children = <code>{children}</code>
+    }
+
+    if (leaf.italic) {
+      children = <em>{children}</em>
+    }
+
+    if (leaf.underline) {
+      children = <u>{children}</u>
+    }
+
+    return <span {...attributes}>{children}</span>
+  };
+  
+  const Element = ({ attributes, children, element, state }) => {
+    const style = { textAlign: element.align }
+    switch (element.type) {
+      case 'block-quote':
+        return (
+          <blockquote
+            style={{
+              fontSize: '1.4em',
+              width: '100%',
+              margin: '0 0',
+              fontFamily: 'Open Sans',
+              fontStyle: 'italic',
+              color: '#555555',
+              padding: '1.2em 1.2rem 1.2em 1.7rem',
+              borderLeft: `3px solid #${state}`,
+              lineHeight: '1.1',
+              position: 'relative',
+              background: '#f3f3f3',
+              style
+            }} {...attributes}>
+            {children}
+          </blockquote>
+        )
+      case 'bulleted-list':
+        return (
+          <ul style={style} {...attributes}>
+            {children}
+          </ul>
+        )
+      case 'heading-one':
+        return (
+          <Heading as='h1' size='xl' noOfLines={1} sx={style} {...attributes}>
+            {children}
+          </Heading>
+        )
+      case 'heading-two':
+        return (
+          <Heading as='h2' size='lg' noOfLines={1} sx={style} {...attributes}>
+            {children}
+          </Heading>
+        )
+      // case 'highlight':
+      //   return (
+      //     <Highlight query={undefined} sx={{px: '1', py: '1', bg: 'blue.300', ...style}} {...attributes}>
+      //       {children}
+      //     </Highlight>
+      //   )
+      case 'list-item':
+        return (
+          <li style={style} {...attributes}>
+            {children}
+          </li>
+        )
+      case 'numbered-list':
+        return (
+          <ol style={style} {...attributes}>
+            {children}
+          </ol>
+        )
+      default:
+        return (
+          <p style={style} {...attributes}>
+            {children}
+          </p>
+        )
+      }
+    };
   return (<Box sx={{
       '& > * > *:nth-of-type(2)': {
         // backgroundColor: 'red',
       }
     }}>
       <Slate editor={editor} value={initialValue}>
-        <Box 
-          display='grid' 
-          gridTemplateColumns='repeat(auto-fit, minmax(max-content, 1.5rem))' 
-          gap='0.5rem'
-          sx={{ 
-            borderLeft: '1px solid #aaa', 
-            borderTop: '1px solid #aaa', 
-            borderRight: '1px solid #aaa', 
-            borderRadius: '0.5rem', 
-            padding: '0.5rem', 
-            '& > *:hover': {
-              transform: 'scale(1.15)'
-            }
-        }}>
-          <MarkButton colorMode={colorMode} icon={<FiBold style={{padding: '0.2rem'}} />} format='bold' />
-          <MarkButton colorMode={colorMode} icon={<FiItalic style={{padding: '0.2rem'}} />} format="italic" />
-          <MarkButton colorMode={colorMode} icon={<FiUnderline style={{padding: '0.2rem'}} />} format="underline" />
-          <MarkButton colorMode={colorMode} icon={<FiCode style={{padding: '0.2rem'}} />} format="code" />
-          <BlockButton colorMode={colorMode} format="heading-one" icon={<TbNumber1 style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="heading-two" icon={<TbNumber2 style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="block-quote" icon={<TbQuote style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="numbered-list" icon={<TbListNumbers style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="bulleted-list" icon={<TbList style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="left" icon={<CiTextAlignLeft style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="center" icon={<CiTextAlignCenter style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="right" icon={<CiTextAlignRight style={{padding: '0.2rem'}} />} />
-          <BlockButton colorMode={colorMode} format="justify" icon={<CiTextAlignJustify style={{padding: '0.2rem'}} />} />
-        </Box>
+        {FormattingBlock}
         <Editable 
           style={{ border: '1px solid #aaa', borderRadius: '0.5rem', padding: '1rem' }}
           spellCheck
@@ -84,144 +157,7 @@ export const DeepWYSIWYG = React.memo<any>(() => {
       </Slate>
     </Box>
   )
-});
-
-const Leaf = ({ attributes, children, leaf }) => {
-  if (leaf.bold) {
-    children = <strong>{children}</strong>
-  }
-
-  if (leaf.code) {
-    children = <code>{children}</code>
-  }
-
-  if (leaf.italic) {
-    children = <em>{children}</em>
-  }
-
-  if (leaf.underline) {
-    children = <u>{children}</u>
-  }
-
-  return <span {...attributes}>{children}</span>
-};
-
-interface BaseProps {
-  className: string
-  [key: string]: unknown
-};
-type OrNull<T> = T | null;
-
-const Button = React.forwardRef(
-  (
-    {
-      colorMode,
-      active,
-      reversed,
-      ...props
-    }: PropsWithChildren<
-      {
-        colorMode?: string
-        active: boolean
-        reversed: boolean
-      } & BaseProps
-    >,
-    ref: Ref<OrNull<HTMLSpanElement>>
-  ) => (
-    <span
-      {...props}
-      ref={ref}
-      style={{
-        cursor: 'pointer',
-        border: 'thin solid #c5c5c5',
-        borderRadius: '0.2rem',
-        backgroundColor: (colorMode === 'dark' && active) 
-          ? '#A8E0FF' 
-          : (colorMode === 'light' && active)
-          ? 'white'
-          : '#E8F1FC',
-        color: reversed
-          ? active
-            ? 'white'
-            : '#aaa'
-          : active 
-            ? '#344055'
-            : 'gray'
-      }}
-    />
-  )
-);
-
-const Icon = React.forwardRef(
-  (
-    { className, ...props }: PropsWithChildren<BaseProps>,
-    ref: Ref<OrNull<HTMLSpanElement>>
-  ) => (
-    <span
-      {...props}
-      ref={ref}
-      style={{
-          fontSize: 24,
-          verticalAlign: 'text-bottom',
-        }}
-    />
-  )
-)
-
-const MarkButton = ({ format, icon, colorMode }) => {
-  const editor = useSlate()
-  return (
-    <Button
-      colorMode={colorMode}
-      active={isMarkActive(editor, format)}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleMark(editor, format);
-        console.log("event", format, event, colorMode);
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
-}
-
-const BlockButton = ({ format, icon, colorMode }) => {
-  const editor = useSlate()
-  return (
-    <Button
-      colorMode={colorMode}
-      active={isBlockActive(
-        editor,
-        format,
-        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
-      )}
-      onMouseDown={event => {
-        event.preventDefault()
-        toggleBlock(editor, format);
-        console.log("event", format, event);
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
-}
-
-const isBlockActive = (editor, format, blockType = 'type') => {
-  const { selection } = editor
-  if (!selection) return false
-
-  const [match] = Array.from(
-    Editor.nodes(editor, {
-      at: Editor.unhangRange(editor, selection),
-      match: n =>
-        !Editor.isEditor(n) &&
-        SlateElement.isElement(n) &&
-        n[blockType] === format,
-    })
-  )
-
-  return !!match
-}
+})
 
 const toggleMark = (editor, format) => {
   const isActive = isMarkActive(editor, format)
@@ -254,7 +190,7 @@ const toggleBlock = (editor, format) => {
       !TEXT_ALIGN_TYPES.includes(format),
     split: true,
   });
-  let newProperties: Partial<SlateElement>
+  let newProperties: Partial<SlateElement>;
   if (TEXT_ALIGN_TYPES.includes(format)) {
     newProperties = {
       // @ts-ignore
@@ -274,50 +210,115 @@ const toggleBlock = (editor, format) => {
   }
 };
 
-const Element = ({ attributes, children, element }) => {
-  const style = { textAlign: element.align }
-  switch (element.type) {
-    case 'block-quote':
-      return (
-        <blockquote style={style} {...attributes}>
-          {children}
-        </blockquote>
-      )
-    case 'bulleted-list':
-      return (
-        <ul style={style} {...attributes}>
-          {children}
-        </ul>
-      )
-    case 'heading-one':
-      return (
-        <h1 style={style} {...attributes}>
-          {children}
-        </h1>
-      )
-    case 'heading-two':
-      return (
-        <h2 style={style} {...attributes}>
-          {children}
-        </h2>
-      )
-    case 'list-item':
-      return (
-        <li style={style} {...attributes}>
-          {children}
-        </li>
-      )
-    case 'numbered-list':
-      return (
-        <ol style={style} {...attributes}>
-          {children}
-        </ol>
-      )
-    default:
-      return (
-        <p style={style} {...attributes}>
-          {children}
-        </p>
-      )
-  }
+const isBlockActive = (editor, format, blockType = 'type') => {
+  const { selection } = editor
+  if (!selection) return false
+
+  const [match] = Array.from(
+    Editor.nodes(editor, {
+      at: Editor.unhangRange(editor, selection),
+      match: n =>
+        !Editor.isEditor(n) &&
+        SlateElement.isElement(n) &&
+        n[blockType] === format,
+    })
+  )
+  return !!match
+}
+
+export const BlockButton = React.memo<any>(({ format, icon, colorMode, setColor }:{ format: any; icon: any; colorMode?: any; setColor?: () => any; }) => {
+  const editor = useSlate();
+  
+  return (
+    <Button
+      colorMode={colorMode}
+      active={isBlockActive(
+        editor,
+        format,
+        TEXT_ALIGN_TYPES.includes(format) ? 'align' : 'type'
+      )}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleBlock(editor, format);
+        if(format === 'block-quote') {
+          setColor();
+        }
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
+});
+
+export const MarkButton = ({ format, icon, colorMode }) => {
+  const editor = useSlate()
+  return (
+    <Button
+      colorMode={colorMode}
+      active={isMarkActive(editor, format)}
+      onMouseDown={event => {
+        event.preventDefault()
+        toggleMark(editor, format);
+      }}
+    >
+      <Icon>{icon}</Icon>
+    </Button>
+  )
 };
+
+const Button = React.forwardRef(({
+  colorMode,
+  active,
+  reversed,
+  ref,
+  ...props
+  }:PropsWithChildren<
+  {
+    colorMode?: string;
+    active: boolean;
+    reversed: boolean;
+    ref: Ref<OrNull<HTMLSpanElement>>;
+  } & BaseProps>,) => (
+    <span
+      {...props}
+      ref={ref}
+      style={{
+        cursor: 'pointer',
+        border: 'thin solid #c5c5c5',
+        borderRadius: '0.2rem',
+        backgroundColor: (colorMode === 'dark' && active) 
+          ? '#A8E0FF' 
+          : (colorMode === 'light' && active)
+          ? 'white'
+          : '#E8F1FC',
+        color: reversed
+          ? active
+            ? 'white'
+            : '#aaa'
+          : active 
+            ? '#344055'
+            : 'gray'
+      }}
+    />
+  )
+);
+
+const Icon = React.forwardRef(({ 
+  className, 
+  ref, 
+  ...props 
+}: PropsWithChildren<
+{
+  className?: any;
+  ref: Ref<OrNull<HTMLSpanElement>>;
+} & BaseProps>,) => (
+    <span
+      {...props}
+      ref={ref}
+      style={{
+          fontSize: 24,
+          verticalAlign: 'text-bottom',
+        }}
+    />
+  )
+)
