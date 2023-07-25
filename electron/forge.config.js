189c856fea01fda970aaf3901c28c5e00e34b389
menzorg <menzorg@gmail.com>
2023-07-25 08:17:57 +0300

    mac build fixes

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   packagerConfig: {
     asar: false,
+    icon: "assets/appIcon.icns",
     osxSign: {
       provisioningProfile: 'developer-id-deepapp.provisionprofile',
       platform: 'mas',
       type: 'development',
       entitlements: 'entitlements.mas.plist',
       hardenedRuntime: true
+    },
+    osxNotarize:{
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLEIDPASS,
+      teamId: process.env.APPLE_TEAM_ID
     }
   },
   rebuildConfig: {},
