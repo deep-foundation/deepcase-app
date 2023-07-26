417c9e2a49c3ffdc9b05bc69f1cbce3addfb3932
menzorg <menzorg@gmail.com>
2023-07-26 22:17:08 +0300

    manual sign

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -3,21 +3,6 @@ module.exports = {
     asar: false,
     icon: "assets/appIcon.icns",
     platform: "mas",
-    osxSign: {
-      platform: "mas",
-      "gatekeeper-assess": false,
-      entitlements: 'entitlements.mas.plist',
-      "entitlements-inherit": "entitlements.mas.inherit.plist",
-      "identity": 'Konstantin Dyachenko (F7AAPNP85N)',
-      provisioningProfile: process.env.PP_PATH,
-      hardenedRuntime: true
-    },
-    osxNotarize:{
-      tool: 'notarytool',
-      appleId: process.env.APPLEID,
-      appleIdPassword: process.env.APPLEIDPASS,
-      teamId: 'F7AAPNP85N'
-    }
   },
   rebuildConfig: {},
   plugins: []
