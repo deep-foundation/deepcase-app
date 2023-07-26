95332dfbc83df9e07e654b8127fdf02a10e5f2e0
menzorg <menzorg@gmail.com>
2023-07-26 08:06:00 +0300

    remove autosign

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -2,11 +2,6 @@ module.exports = {
   packagerConfig: {
     asar: false,
     icon: "assets/appIcon.icns",
-    osxSign: {
-      provisioningProfile: `${process.env.RUNNER_TEMP}/developer-id-deepapp.provisionprofile`,
-      entitlements: 'entitlements.mas.plist',
-      hardenedRuntime: true
-    },
   },
   rebuildConfig: {},
   makers: [
