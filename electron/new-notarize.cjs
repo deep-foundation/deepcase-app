380c4e3878e2c151cb090e0f41d4796eb1269e59
menzorg <menzorg@gmail.com>
2023-07-27 01:45:14 +0300

    rebuild macos

--- electron/new-notarize.cjs
+++ electron/new-notarize.cjs
@@ -1,10 +1,15 @@
 const { notarize } = require('electron-notarize');
+const fs = require('fs');
+
+console.log(fs.existsSync('./out'));
+console.log(fs.existsSync('./out/Deep.Case-darwin-x64'));
+console.log(fs.existsSync('./out/Deep.Case-darwin-x64/Deep.Case.app'));
 
 const appBundleId = 'deep.app';
 
 console.log({
   appBundleId,
-  appPath: `out/Deep.Case-darwin-x64/Deep.Case.app`,
+  appPath: `./out/Deep.Case-darwin-x64/Deep.Case.app`,
   appleId: process.env.APPLEID,
   appleIdPassword: process.env.APPLEIDPASS,
 });
