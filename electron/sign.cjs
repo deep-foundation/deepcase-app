dcbd2c6c525b3034dd3ab18eff5b34bfbeb4d820
Пётр Безденежных <menzorg@gmail.com>
2023-07-26 05:08:51 +0300

    mac rebuild

---
+++ electron/sign.cjs
@@ -0,0 +1,13 @@
+const { signAsync } = require('@electron/osx-sign')
+signAsync({
+  app: 'Deep.Case.app'
+  provisioningProfile: `${process.env.RUNNER_TEMP}/developer-id-deepapp.provisionprofile`,
+  entitlements: 'entitlements.mas.plist',
+  hardenedRuntime: true
+})
+  .then(function () {
+    console.log('sign complete');
+  })
+  .catch(function (err) {
+    console.log('sign error', err);
+  })
