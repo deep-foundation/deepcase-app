dd532f60f616031bae297b4ddce14172042e41bb
menzorg <menzorg@gmail.com>
2023-07-26 08:48:30 +0300

    try dmg sign

--- electron/forge.config.js
+++ electron/forge.config.js
@@ -2,6 +2,12 @@ module.exports = {
   packagerConfig: {
     asar: false,
     icon: "assets/appIcon.icns",
+    osxNotarize:{
+      tool: 'notarytool',
+      appleId: process.env.APPLEID,
+      appleIdPassword: process.env.APPLEIDPASS,
+      teamId: 'F7AAPNP85N'
+    }
   },
   rebuildConfig: {},
   makers: [
@@ -9,6 +15,11 @@ module.exports = {
       name: '@electron-forge/maker-dmg',
       config: {
         format: 'ULFO'
+        additionalDMGOptions:{
+          "code-sign": {
+            "signing-identity": 'Konstantin Dyachenko (F7AAPNP85N)'
+          }
+        }
       }
     }
   ],
